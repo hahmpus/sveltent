213c19792fb4b553ab3ed2c28d03de87484da53f
hampus <hampus@DESKTOP-B873QV7>
2024-06-03 22:30:35 +0200

    new layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,26 +4,49 @@
 
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
-	import { AppBar, AppShell, storePopup } from '@skeletonlabs/skeleton';
+	import { AppRail, AppRailAnchor, AppRailTile, AppShell, storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 	
 	/** @type {import('./$types').LayoutData} */
 	export let data;
+
+    let currentTile = 0;
 </script>
 
 <div>
 	<AppShell>
     
-        <AppBar slot="header">
-            <a href="/" class="btn variant-filled-primary">
+        <!-- (header) -->
+
+        <AppRail slot="sidebarLeft">
+            <!-- <a href="/" class="btn variant-filled-primary">
                 Home
             </a>
             <a href="/recipie" class="btn variant-filled-primary">
                 Recipies
-            </a>
-        </AppBar>
+            </a> -->
+            <svelte:fragment slot="lead">
+                <AppRailAnchor href="/" >(icon)</AppRailAnchor>
+            </svelte:fragment>
+            <!-- --- -->
+            <AppRailTile bind:group={currentTile} name="tile-1" value={0} title="tile-1">
+                <svelte:fragment slot="lead">(icon)</svelte:fragment>
+                <span>Tile 1</span>
+            </AppRailTile>
+            <AppRailTile bind:group={currentTile} name="tile-2" value={1} title="tile-2">
+                <svelte:fragment slot="lead">(icon)</svelte:fragment>
+                <span>Tile 2</span>
+            </AppRailTile>
+            <AppRailTile bind:group={currentTile} name="tile-3" value={2} title="tile-3">
+                <svelte:fragment slot="lead">(icon)</svelte:fragment>
+                <span>Tile 3</span>
+            </AppRailTile>
+            <!-- --- -->
+            <svelte:fragment slot="trail">
+                <AppRailAnchor href="/" target="_blank" title="Account">(icon)</AppRailAnchor>
+            </svelte:fragment>
+        </AppRail>
 
-        <!-- (sidebarLeft) -->
         <!-- (sidebarRight) -->
         <!-- (pageHeader) -->
 
