f2bc13841f0cfa09a2ed0a6bb07c3c6bca2fa2e9
hampus <hampus@DESKTOP-B873QV7>
2024-06-04 21:04:45 +0200

    package update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,8 @@
 	import '../app.postcss';
 
 	// Floating UI for Popups
-	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
-	import { AppRail, AppRailAnchor, AppRailTile, AppShell, storePopup } from '@skeletonlabs/skeleton';
-	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+	import { AppBar, AppRail, AppRailAnchor, AppRailTile } from '@skeletonlabs/skeleton';
+
 	
 	/** @type {import('./$types').LayoutData} */
 	export let data;
@@ -14,49 +13,23 @@
 </script>
 
 <div>
-	<AppShell>
-    
-        <!-- (header) -->
 
-        <AppRail slot="sidebarLeft">
-            <!-- <a href="/" class="btn variant-filled-primary">
-                Home
-            </a>
-            <a href="/recipie" class="btn variant-filled-primary">
-                Recipies
-            </a> -->
-            <svelte:fragment slot="lead">
-                <AppRailAnchor href="/" >(icon)</AppRailAnchor>
-            </svelte:fragment>
-            <!-- --- -->
-            <AppRailTile bind:group={currentTile} name="tile-1" value={0} title="tile-1">
+    <div class="bg-surface-100-800-token">
+        <div class="mx-auto max-w-6xl">
+            <AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
                 <svelte:fragment slot="lead">(icon)</svelte:fragment>
-                <span>Tile 1</span>
-            </AppRailTile>
-            <AppRailTile bind:group={currentTile} name="tile-2" value={1} title="tile-2">
-                <svelte:fragment slot="lead">(icon)</svelte:fragment>
-                <span>Tile 2</span>
-            </AppRailTile>
-            <AppRailTile bind:group={currentTile} name="tile-3" value={2} title="tile-3">
-                <svelte:fragment slot="lead">(icon)</svelte:fragment>
-                <span>Tile 3</span>
-            </AppRailTile>
-            <!-- --- -->
-            <svelte:fragment slot="trail">
-                <AppRailAnchor href="/" target="_blank" title="Account">(icon)</AppRailAnchor>
-            </svelte:fragment>
-        </AppRail>
+                (title)
+                <svelte:fragment slot="trail">(actions)</svelte:fragment>
+            </AppBar>
+        </div>
+    </div>
 
-        <!-- (sidebarRight) -->
-        <!-- (pageHeader) -->
-
+    <div class="mx-auto max-w-6xl">
 		<PageTransition key={data.pathname}>
 			<div class="container h-full mx-auto justify-center">
                 <slot />
 			</div>
 		</PageTransition>
+    </div>
 
-
-        <!-- (footer) -->
-    </AppShell>
 </div>
